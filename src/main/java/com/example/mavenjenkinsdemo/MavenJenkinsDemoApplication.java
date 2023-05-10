a065c725a49d3af85322b3572c2ee5713dcbd7c2
Vishvas Pandya <vishvas.pandya@techforce.com>
2023-05-10 16:14:06 +0530

    Extended SpringBootServletInitializer in Application class

--- src/main/java/com/example/mavenjenkinsdemo/MavenJenkinsDemoApplication.java
+++ src/main/java/com/example/mavenjenkinsdemo/MavenJenkinsDemoApplication.java
@@ -2,9 +2,10 @@ package com.example.mavenjenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MavenJenkinsDemoApplication {
+public class MavenJenkinsDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenJenkinsDemoApplication.class, args);
